29cd21a08a95cbe7bd84aceb62ead9a961d6bbee
xabner233 <abner233.aso@gmail.com>
2023-09-28 15:23:17 -0300

    Initial commit

---
+++ src/main/java/me/dio/santanderbootcamp2023/SantanderBootcamp2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.santanderbootcamp2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	SantanderBootcamp2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcamp2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santanderbootcamp2023/SantanderBootcamp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderbootcamp2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcamp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
